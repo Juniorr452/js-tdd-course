6649e3fbf8378c239b01134e57f21794db5ea13a
Junior <juniorr452@gmail.com>
2019-07-29 09:30:22 -0300

    Mini lib de calc

--- 05-unindo-conceitos-na-pratica/src/calc.js
+++ 05-unindo-conceitos-na-pratica/src/calc.js
@@ -1,6 +1,6 @@
 module.exports = {
-  sum: () => {},
-  sub: () => {},
-  mult: () => {},
-  div: () => {}
+  sum: (a, b) => a + b,
+  sub: (a, b) => a - b,
+  mult: (a, b) => a * b,
+  div: (a, b) => b === 0 ? 'Não é possível divisão por zero' : a / b
 }
